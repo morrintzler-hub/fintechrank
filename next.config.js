c579d5e44faaf1c118081ce21a3dd19c1322d6ba
“morrintzler-hub” <mor.rintzler@gmail.com>
2026-06-02 10:12:54 +0300

    Fix readability colors and logo domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['xkdiludpiwtihogyikg.supabase.co'],
+    domains: ['logo.clearbit.com'],
   },
 }
 module.exports = nextConfig
